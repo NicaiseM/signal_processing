0434eabdeb3d355ba74e390a30bb15f1f8090059
NicaiseM <nicaise@rambler.ru>
2022-05-23 10:53:26 +0300

    Project created.

---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Signal processing
+
+Oscilloscope waveform processing script.
+"""
+
+# Future
+# from __future__ import
+
+# Standard Library
+import os
+import time
+from pathlib import Path
+
+# Third-party Libraries
+import scipy
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Own sources
+from metric import metric as mt
+
+__author__      = "Nikita Makarchuck"
+__copyright__   = "Copyright 2021, Nikita Makarchuck"
+__credits__     = ["Nikita Makarchuck"]
+__license__     = "GPL"
+__version__     = "1.0.0"
+__maintainer__  = "Nikita Makarchuck"
+__email__       = "nicaise@rambler.ru"
+__status__      = "Prototype|Development|Production"
+
+
+# === Processing ===
+
+# TODO
+    # открытие файлов
+    # обработка
+    # графопостроение
+
